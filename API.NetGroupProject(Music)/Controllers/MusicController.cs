b83e628621ce13c19bffc6b55174b15e4ca38ae8
synthrootcoding <synthrootcoding@gmail.com>
2020-11-28 10:20:17 -0500

    added methods for favorites

--- API.NetGroupProject(Music)/Controllers/MusicController.cs
+++ API.NetGroupProject(Music)/Controllers/MusicController.cs
@@ -1,4 +1,5 @@
 ﻿using API.NetGroupProject_Music_.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ namespace API.NetGroupProject_Music_.Controllers
     public class MusicController : Controller
     {
         private readonly MusicDAL _dal;
+        private readonly MusicProjectDbContext _db = new MusicProjectDbContext();
         public MusicController(MusicDAL dal)
         {
             _dal = dal;
@@ -18,6 +20,40 @@ namespace API.NetGroupProject_Music_.Controllers
         {
             var result = await _dal.GetSearchAsync();
             return View(result);
+        }
+
+        public IActionResult Favorites()
+        {
+            return View(_db.Favorites.ToList());
+        }
+
+        [Authorize]
+        public IActionResult UserFavorites()
+        {
+            return View(_db.UserFavorites.ToList());
+        }
+
+
+        [HttpPost]
+        public IActionResult RemoveFavorite (Favorites f)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Favorites.Remove(f);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("/MusicFavorites");
+        }
+
+        [HttpPost]
+        public IActionResult AddFavorite (Favorites f)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Favorites.Add(f);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Music/Favorites");
         }
 
         public IActionResult MusicSearch()
--- API.NetGroupProject(Music)/Models/Favorites.cs
+++ API.NetGroupProject(Music)/Models/Favorites.cs
@@ -6,5 +6,8 @@ namespace API.NetGroupProject_Music_.Models
     public partial class Favorites
     {
         public int Id { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
     }
 }
