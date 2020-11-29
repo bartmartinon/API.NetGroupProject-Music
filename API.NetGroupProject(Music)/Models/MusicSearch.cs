bb180261f9566ae81c7b5e343a661f34daefa4d3
duncaannn <drbigfupa@yahoo.com>
2020-11-29 12:40:50 -0500

    sunday

--- API.NetGroupProject(Music)/Models/MusicSearch.cs
+++ API.NetGroupProject(Music)/Models/MusicSearch.cs
@@ -93,5 +93,35 @@ namespace API.NetGroupProject_Music_.Models
     }
 
 
+    public class TrackList
+    {
+        public int id { get; set; }
+        public bool readable { get; set; }
+        public string title { get; set; }
+        public string title_short { get; set; }
+        public string title_version { get; set; }
+        public string isrc { get; set; }
+        public string link { get; set; }
+        public string share { get; set; }
+        public int duration { get; set; }
+        public int track_position { get; set; }
+        public int disk_number { get; set; }
+        public int rank { get; set; }
+        public string release_date { get; set; }
+        public bool explicit_lyrics { get; set; }
+        public int explicit_content_lyrics { get; set; }
+        public int explicit_content_cover { get; set; }
+        public string preview { get; set; }
+        public int bpm { get; set; }
+        public float gain { get; set; }
+        public string[] available_countries { get; set; }
+        public Contributor[] contributors { get; set; }
+        public string md5_image { get; set; }
+        public Artist artist { get; set; }
+        public Album album { get; set; }
+        public string type { get; set; }
+    }
+
+
 
 }
