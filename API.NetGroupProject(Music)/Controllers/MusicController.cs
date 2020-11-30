59a8a84a5d851d6f707961321f1fc5a7d1e32e4b
Kristen Harrell <71024265+kristen-harrell@users.noreply.github.com>
2020-11-30 14:46:28 -0500

    final update to controller to correct all view routings

--- API.NetGroupProject(Music)/Controllers/MusicController.cs
+++ API.NetGroupProject(Music)/Controllers/MusicController.cs
@@ -27,7 +27,7 @@ namespace API.NetGroupProject_Music_.Controllers
 
                 var result = await _dal.GetMusicAsync(data);
 
-                return View("MusicSearchResultsAlbum", result);
+                return View("showalbums", result);
             }
             if (SearchBy == "song")
             {
@@ -94,6 +94,40 @@ namespace API.NetGroupProject_Music_.Controllers
 
             return View("TracklistDetails", result);
         }
+        public async Task<IActionResult> MusicSearchLinkAsync(string data, string SearchBy)
+        {
+            if (SearchBy == "artist")
+            {
+                ViewBag.Artist = data.ToLower();
 
+                var result = await _dal.GetMusicAsync(data);
+
+                return View("MusicSearchResultsArtist", result);
+            }
+            if (SearchBy == "album")
+            {
+                ViewBag.Album = data.ToLower();
+
+                var result = await _dal.GetMusicAsync(data);
+
+                return View("MusicSearchResultsAlbum", result);
+            }
+            if (SearchBy == "song")
+            {
+                ViewBag.Track = data.ToLower();
+
+                var result = await _dal.GetMusicAsync(data);
+
+                return View("MusicSearchResultsTrack", result);
+            }
+            else
+                return View("index");
+        }
+        public async Task<IActionResult> AlbumSearchLinkAsync(int albumId)
+        {
+            var result = await _dal.GetAlbumAsync(albumId);
+
+            return View("TracklistDetails", result);
+        }
     }
 }
